3719683f17eb7fe883a39faecdff877d92f612e0
brightmanforever022 <brightmanforever@outlook.com>
2020-04-26 15:54:50 -0700

    updates

--- pages/help.js
+++ pages/help.js
@@ -4,9 +4,11 @@ const Help = () => {
   return (
     <Page title="Help Center. We are here for you.">
       <Layout>
-        <Card title="We are here for you. Connect with us.">
-          <p>Send in your ticket and we will solve it in blazing fast speed.</p>
-        </Card>
+        <Layout.Section>
+          <Card title="We are here for you. Connect with us." sectioned>
+            <p>Send in your ticket and we will solve it in blazing fast speed.</p>
+          </Card>
+        </Layout.Section>
       </Layout>
     </Page>
   )
