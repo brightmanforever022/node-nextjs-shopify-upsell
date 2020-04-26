36ead34401c103b714cc0d38e6a58ca40439979b
brightmanforever022 <brightmanforever@outlook.com>
2020-04-26 15:21:53 -0700

    updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,29 @@
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
-import App, { Container } from "next/app";
-import { AppProvider } from "@shopify/polaris";
-import { Provider } from "@shopify/app-bridge-react";
-import Cookies from "js-cookie";
-import "@shopify/polaris/styles.css";
-import translations from "@shopify/polaris/locales/en.json";
-
-const apiKey = process.env.SHOPIFY_API_KEY;
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import App, { Container } from 'next/app';
+import { AppProvider } from '@shopify/polaris';
+import { Provider } from '@shopify/app-bridge-react';
+import Cookies from 'js-cookie';
+import '@shopify/polaris/styles.css';
+import translations from '@shopify/polaris/locales/en.json';
 
 const client = new ApolloClient({
   fetchOptions: {
-    credentials: "include"
+    credentials: 'include'
   }
 });
+
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
-    const shopOrigin = Cookies.get("shopOrigin");
+    // const shopOrigin = Cookies.get('shopOrigin');
+    const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
+
     return (
       <Container>
         <AppProvider i18n={translations}>
           <Provider
-            config={{
-              apiKey: apiKey,
-              shopOrigin: shopOrigin,
-              forceRedirect: true
-            }}
+            config={config}
           >
             <ApolloProvider client={client}>
               <Component {...pageProps} />
