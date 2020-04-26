5e4d1ed5953bdc18e797ebf981ace6e16c78e470
brightmanforever022 <brightmanforever@outlook.com>
2020-04-26 15:46:39 -0700

    adding help page

---
+++ pages/help.js
@@ -0,0 +1,15 @@
+import { Page, Layout, Card } from '@shopify/polaris';
+
+const Help = () => {
+  return (
+    <Page title="Help Center. We are here for you.">
+      <Layout>
+        <Card title="We are here for you. Connect with us.">
+          <p>Send in your ticket and we will solve it in blazing fast speed.</p>
+        </Card>
+      </Layout>
+    </Page>
+  )
+}
+
+export default Help;
