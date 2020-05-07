2d5ce2e5fb438808fc66db9e23d20affbbe1cd89
brightmanforever022 <brightmanforever@outlook.com>
2020-05-07 00:40:42 -0700

    updates

--- components/EnableTipJarApp.js
+++ components/EnableTipJarApp.js
@@ -6,7 +6,7 @@ function EnableTipJarApp({ settings, updateSettings }) {
 
   const handleToggle = useCallback(() => {
     setTipJarActive((tipJarActive) => !tipJarActive);
-    let newSettings = settings;
+    let newSettings = [...settings];
     newSettings.enableTipJar = tipJarActive;
     updateSettings(newSettings);
   }, []);
