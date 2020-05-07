67e5034697d040be8f71a9d9ad969be94e08dc29
brightmanforever022 <brightmanforever@outlook.com>
2020-05-07 15:21:28 -0700

    updates

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import EnableTipJarApp from '../components/EnableTipJarApp';
 import EnableCustomTipOption from '../components/EnableCustomTipOption';
 import TipModalTitle from '../components/TipModalTitle';
 import TipModalDescription from '../components/TipModalDescription';
+import initialSettings from '../utils/initialSettings';
 
 const SHOP_TIPJAR_METAFIELD_QUERY = gql`
   query {
@@ -51,6 +52,22 @@ const Index = () => {
     setUpdateMetafieldIsLoading(false);
     refetch();
   }
+
+  const handleInstallInitialSettings = async () => {
+    const updateMetafield = await fetch('/updateSettingsMetafield', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        metafieldValue: JSON.stringify(initialSettings)
+      })
+    });
+    const updateMetafieldJson = await updateMetafield.json();
+    console.log('Response for updateMetafieldJson:', JSON.stringify(updateMetafieldJson));
+
+    refetch();
+  }
   
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
@@ -69,7 +86,11 @@ const Index = () => {
     >
       {!originalSettings &&
         <Layout>
-          There are no shop settings yet, click here to install.
+          <Card sectioned title="Installation instructions">
+            1. Create the snippet and include at end of body tag.
+            2. Install initial settings.
+            <Button action={() => handleInstallInitialSettings}></Button>
+          </Card>
         </Layout>
       }
 
---
+++ utils/initialSettings.js
@@ -0,0 +1,9 @@
+export default {
+  'enableTipJar': false,
+  'enableCustomTipOption': false,
+  'tipModalTitle': 'Leave a tip',
+  'tipModalDescription': 'Lorem ipsum',
+  'defaultTipping15': true,
+  'defaultTipping20': true,
+  'defaultTipping25': true,
+}
