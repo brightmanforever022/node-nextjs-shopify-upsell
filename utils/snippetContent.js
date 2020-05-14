0b21c8b70f8ee2193602cfac84d110b240d28291
brightmanforever022 <brightmanforever@outlook.com>
2020-05-14 14:55:31 -0700

    updates

--- utils/snippetContent.js
+++ utils/snippetContent.js
@@ -261,6 +261,10 @@ export default `{% for key_value in shop.metafields.tipjar.settings %}
   .tj-modal-custom-input {
     min-width: 1px;
     flex-grow: 1;
+    margin: 0;
+    border: 1px solid #999999;
+    border-radius: 5px;
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
   }
   
   @media only screen and (min-width: 480px) {
