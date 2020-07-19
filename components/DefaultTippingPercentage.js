16ad1c45b76ff9adcc0958a3165b3e63e3e8692d
brightmanforever022 <brightmanforever@outlook.com>
2020-07-19 16:02:23 +0800

    Fixes the prefix in tip percentage fields

--- components/DefaultTippingPercentage.js
+++ components/DefaultTippingPercentage.js
@@ -44,18 +44,21 @@ function DefaultTippingPercentage({ settings, updateSettings }) {
             <TextField
               label="Tip Amount #1"
               type="number"
+              prefix="%"
               value={defaultTipping1}
               onChange={handleChange1}
             />
             <TextField
               label="Tip Amount #2"
               type="number"
+              prefix="%"
               value={defaultTipping2}
               onChange={handleChange2}
             />
             <TextField
               label="Tip Amount #3"
               type="number"
+              prefix="%"
               value={defaultTipping3}
               onChange={handleChange3}
             />
